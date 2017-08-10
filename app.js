811116e7a3214d69e6a5d37e956d3b1a8f038235
Raghav Ramesh <raghavramesh94@gmail.com>
2017-08-10 21:06:01 +0800

    Init + Added init files. + Added .gitignore and ignored those within node_modules/

---
+++ app.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var fs = require('fs');
+var app = express();
+
+// TODO Implement your code here.
+// You can use the sample data here: https://demo2697834.mockable.io/movies
+// or use your own data
+
+app.listen(3000, function () {
+	console.log('Server listening on port 3000');
+});
