76c7996407cdf707fe32fdd6f7939cb2559107e1
Raghav Ramesh <raghavramesh94@gmail.com>
2017-08-16 10:04:16 +0800

    Refactor + Fixed comments.

--- app.js
+++ app.js
@@ -3,21 +3,19 @@ var fs = require('fs');
 var request = require('request');
 var app = express();
 
-// TODO Implement your code here.
-// You can use the sample data here: https://demo2697834.mockable.io/movies
-// or use your own data
-app.all('/*', function(req, res, next) {
+// Adds the following headers to the response when a request to /api/videos is made
+app.all('/api/videos', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "http://localhost:8080");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With"); // TODO
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
   next();
 });
 
+// GETs data from the demo API and sends the response from the API in the GET response body
 app.get('/api/videos', function(req, res) {
   request('https://demo2697834.mockable.io/movies', function(err, response, body) {
     res.send(body);
   });
 });
-
 
 app.listen(3000, function () {
 	console.log('Server listening on port 3000');
